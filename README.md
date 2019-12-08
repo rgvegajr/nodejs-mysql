91f5bfe2a22ceb3122e74dbc294d65df896e5ff8
RV <rgvegajr@gmail.com>
2019-12-08 17:51:37 -0500

    fully functional bamazon customer app and complete readme with screenshots

--- README.md
+++ README.md
@@ -1,6 +1,57 @@
-# nodejs-mysql
+# nodejs-mysql app 
+### (Note: this file is mark-down formatted to be read in a code-editor "preview" function or on github)
+## Welcome to the readme file for my nodejs-mysql app!  This application is available at the following github repository:  
 
-1.  Create mysql db with product info (id, item, cost, quantity)
-2.  edit .js file to connect to db
-3.  develop functions in .js file to do CRUD on db
-4.  develop queries using inquirer to interact with user - desired item from id, and number to buy
+* https://github.com/rgvegajr/nodejs-mysql
+
+## Background/Purpose:
+
+This command-line application incorporates a MySQL database with nodejs to demonstrate read and update functionality of the database via a command-line interface using a "bamazon" storefront.  The node.js filename is bamazonCustomer.js which, when run, generates a command-line user interface with a table of items to purchase, user prompts for item to buy and quantity desired (or to enter Q to quit) and includes rudimentary input data verification.
+
+## Technologies used:
+
+node.js, with inquirer and mysql npm packages installed.  npm inquirer provides the user prompt funcitonality. npm mysql provides the database manupulation functionality.
+
+## My role:  
+
+I am the app developer.  I used MS Visual Studio Code as my integrated development environment and Github for repository and version control.  Bootcamp instructors provided guidelines, instructions and requirements for this app.
+
+## App Organization:
+
+The code is organized as follows:
+1.  Required modules import
+2.  Global variables declared
+3.  Function declarations to enable a customer to view items available for purchase, enter a selected item id and purchase quantity which results in a purchase total and a decrement of item stock.
+4.  Function call to generate comandline based user interface and prompts.
+
+## Instructions (These instructions assume user is familiar with GITHUB and node js and has installed node.js on a PC/Mac.):
+
+1.  Open a terminal/bash window on your PC/Mac.
+2.  Clone the GITHUB repository for this project from.  Enter into terminal: git clone https://github.com/rgvegajr/nodejs-mysql.git
+3.  Ensure you are in the nodejs-mysql directory and run command "npm i" to install all required packages.
+4.  Obtain database credentials from the author or alternatively obtain a copy of the database or import the storeItems_db from the repo, install locally, and edit the bamazonCustomer.js database connection code to enable database functionality.
+5.  Run the program by entering "node bamazonCustomer.js"
+6.  Select a desired item number and quantity per the prompts.  If item exists and sufficient quantity is available, the CLI app will return a succesful purchase and amount in US Dollars. If insufficient quantity exists, the app will notify the user and return to the item prompt. If item does not exist, user will be notified and asked to choose an existing item.  If user enters "Q" (or "q"),application will quit, notifying the user with a "Goodbye!".
+
+
+## Screenshots:
+
+User Interface
+
+![user-interface](ui.png)
+
+Successful purchase:
+
+![good-purchase](gp.png)
+
+Insufficient Quantity:
+
+![insufficient-quantity](iq.png)
+
+Item Does Not Exist:
+
+![item-DNE](idne.png)
+
+Quit:
+
+![quit](quit.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,76 +1,96 @@
+//require packages and set global variables
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 //would normally use dotenv to handle keys, pws, etc
 
-// const connection = mysql.createConnection({ //creatConnection function takes in object with host, port, un, pw, db name, etc
-//     host: "localhost", //url where databse lives
+//user should edit and save the following database connection code with appropriate host,port,user,password, and database specifics
+const connection = mysql.createConnection({ //creatConnection function takes in object with host, port, un, pw, db name, etc
+    host: "localhost", //url where databse lives
+    // your port; if not 3306
+    port: 3306,
+    // your username
+    user: "root",
+    //yourpassword
+    password: "78652145",
+    database: "storeItems_db"
+});
 
-//     // your port; if not 3306
-//     port: 3306,
+//declare functions
+function updateItem(item_id, stock_quantity, numberToBuy) {
+    let updatedQuantity = stock_quantity - numberToBuy;
+    const query = connection.query(
+        "UPDATE items SET stock_quantity=" + updatedQuantity + " WHERE item_id = " + item_id + ";",
+        function(err, res) {
+            if (err) throw err;
+        }
+    );
+}
 
-//     // your username
-//     user: "root",
+function askBuyerItem(itemArr) {
+    inquirer
+        .prompt([
+            // prompt user for desired item id.
+            {
+                type: "input",
+                message: "What is the id of the item you would like to purchase?[Quit with Q]",
+                name: "itemId"
+            }
+        ])
+        .then(function(inquirerResponse) {
+            if ((inquirerResponse.itemId == "q") || (inquirerResponse.itemId == "Q")) {
+                console.log("Goodbye!");
+                connection.end();
+                return;
+            } else if (inquirerResponse.itemId > itemArr.length) {
+                console.log("Sorry, item does not exist.  Please choose an existing item.");
+                askBuyerItem(itemArr);
+            } else {
+                const index = inquirerResponse.itemId - 1;
+                askBuyerNumber(itemArr, index);
+            }
+        });
+}
 
-//     //yourpassword
-//     password: "",
+function askBuyerNumber(itemArr, index) {
+    inquirer
+        .prompt([
+            // prompt user for number to buy.
+            {
+                type: "input",
+                message: "How many would you like to purchase?[Quit with Q]",
+                name: "numberToBuy"
+            }
+        ])
+        .then(function(inquirerResponse) {
+            if ((inquirerResponse.numberToBuy == "q") || (inquirerResponse.numberToBuy == "Q")) {
+                console.log("Goodbye!");
+                connection.end();
+                return;
+            } else
+            if (inquirerResponse.numberToBuy <= itemArr[index].stock_quantity) {
+                console.log("--------------------------------------------------------------------");
+                console.log("\nSuccessfully purchased " + inquirerResponse.numberToBuy + " of " + itemArr[index].product_name + " for a total cost of $" + ((itemArr[index].price * inquirerResponse.numberToBuy)).toFixed(2) + ".");
+                updateItem(itemArr[index].item_id, itemArr[index].stock_quantity, inquirerResponse.numberToBuy);
+            } else {
+                console.log("--------------------------------------------------------------------");
+                console.log("\nInsufficient quantity!");
+            };
+            console.log("\n--------------------------------------------------------------------");
+            afterConnection();
+        });
+}
 
-//     database: "ice_creamDB"
-// });
-
-// connection.connect(function(err) {    //before calling any other functions to query database, etc, must ensure a connection
-//     if (err) throw err;
-//     console.log(`connected as id ${connection.threadId}`);
-//     connection.end(); //closes connection to db, critical to always do...or replace this with desired function and ensure this line is included at the end of the function
-//     afterConnection();
-// });
-
-// function afterConnection() {
-    //     connection.query("SELECT * FROM products", function(err, res) {
-    //         if (err) throw err;
-    //         console.log(res);
-    //         connection.end();
-    //     });
-    // }
-
-
-
-
-
-// Create a "Prompt" with a series of questions.
-inquirer
-    .prompt([
-    // prompt user for desired item id.
-        {
-            type: "input",
-            message: "What is the id of the item you would like to purchase?",
-            name: "itemId"
-    },
-        {
-            type: "input",
-            message: "How many would you like to purchase?",
-            name: "numberToBuy"
-    }
-    // Here we create a basic password-protected text prompt.
-    // {
-    //   type: "password",
-    //   message: "Set your password",
-    //   name: "password"
-    // },
-    // // Here we give the user a list to choose from.
-    // {
-    //   type: "list",
-    //   message: "Which Pokemon do you choose?",
-    //   choices: ["Bulbasaur", "Squirtle", "Charmander"],
-    //   name: "pokemon"
-    // },
-    // // Here we ask the user to confirm.
-    // {
-    //   type: "confirm",
-    //   message: "Are you sure:",
-    //   name: "confirm",
-    //   default: true
-    // }
-  ])
-    .then(function(inquirerResponse) {
-        console.log("\nYou chose to purchase " + inquirerResponse.numberToBuy + " of " + inquirerResponse.itemId);
+function afterConnection() {
+    connection.query("SELECT * FROM items", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        const itemArr = Object.values(res);
+        askBuyerItem(itemArr);
     });
+}
+
+//establish connection and call functions
+connection.connect(function(err) { //before calling any other functions to query database, etc, must ensure a connection
+    if (err) throw err;
+    afterConnection();
+});
